ae0b51676c54c71beffdf1788af52cc0eea964c6
Piyush <piyush.ag.bitsg@gmail.com>
2021-06-14 21:48:03 +0530

    Fixed build.gradle *Updated Employee Model

--- src/main/java/com/example/employeemanagement/model/Employee.java
+++ src/main/java/com/example/employeemanagement/model/Employee.java
@@ -1,17 +1,22 @@
 package com.example.employeemanagement.model;
 
+import com.example.employeemanagement.enums.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Document(collection = "employee")
 public class Employee {
     @Id
     private String id;
     private String name;
+    private int age;
+    private Gender gender;
 }
